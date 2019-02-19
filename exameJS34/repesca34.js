a07b76599079b6e9d01b7981fea38dec37b21607
a319115cv <44172458+a319115cv@users.noreply.github.com>
2019-02-19 17:36:45 +0100

    Add files via upload

---
+++ exameJS34/repesca34.js
@@ -0,0 +1,48 @@
+document.write('Espero que sexa bo este  xogo')
+prompt('Xogaremos a un xogo, podes elegir se queres sumar ou multiplicar, o que queiras xogar so tes que escribilo')
+if= ('sumar') {
+	x= prompt('Vale, 1. 1+1= ?')
+	while(x !='2'){
+	alert('Fallaste')
+	x= prompt('1. 1+1= ?')
+	}
+}
+if= ('multiplicar') {
+	y= prompt('Guay, 1. 1*2= ?')
+	while(y !='2'){
+	alert('Fallaste')
+	y= prompt('1. 1*2= ?')
+	}
+}
+prompt('Outra máis, pon entre sumar ou multiplicar o que queres agora')
+if= ('sumar') {
+	b= prompt('Vale, 2. 2+2= ?')
+	while(b !='4'){
+	alert('Fallaste')
+	b= prompt('2. 1+1= ?')
+	}
+}
+if= ('multiplicar') {
+	z= prompt('Guay, 2. 3*2= ?')
+	while(z !='6'){
+	alert('Fallaste')
+	z= prompt('2. 1*2= ?')
+	}
+}
+
+prompt('A ultima,sumar ou multiplicar, desta sera mais complicado')
+if= ('sumar') {
+	// Esto lo hago para crear un bucle
+	b= prompt('Vale, 3. 5+15= ?')
+	while(b !='20'){
+	alert('Fallaste')
+	b= prompt('3. 5+15= ?')
+	}
+}
+if= ('multiplicar') {
+	z= prompt('Ben, 3. 3*5= ?')
+	while(z !='15'){
+	alert('Fallaste')
+	z= prompt('3. 3*5= ?')
+	}
+}
